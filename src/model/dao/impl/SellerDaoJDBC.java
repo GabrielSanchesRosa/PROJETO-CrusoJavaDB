35c0aa97b6c7b60ba53e3d1f54de6ca3f77e798a
Gabriel Sanches <gabrielsanches131202@gmail.com>
2023-02-11 18:36:15 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -10,6 +10,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.crypto.ShortBufferException;
+
+import com.mysql.cj.x.protobuf.MysqlxExpr.Identifier;
+
 import db.DB;
 import db.DbException;
 import model.dao.SellerDao;
@@ -81,9 +85,20 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public void deleteById(Integer obj) {
+	public void deleteById(Integer id) {
+		PreparedStatement st = null;
 		
-		
+		try {
+			st = conn.prepareStatement("Delete from SELLER Where Id = ?");
+			
+			st.setInt(1, id);
+			
+			st.executeUpdate();
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
